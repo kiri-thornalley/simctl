56fec9e1c49cbdc5ccb54fae844a88243731ca67
Kiri Thornalley <143103370+kiri-thornalley@users.noreply.github.com>
2026-09-07 19:09:20 +0100

    feat commit script to run tests, vet, staticcheck

---
+++ scripts/gocheck.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+
+# Exit immediately if a command exits with a non‑zero status.
+set -euo pipefail
+
+# Format Go code
+go fmt ./...
+
+# Does this code compile?
+go build -o simctl ./cmd/simctl
+
+# Run tests for the current module and all its sub‑packages -- Do the tests pass?
+go test -v ./...
+
+# Run tests with race detector, detects concurrency bugs
+go test -race -v ./...
+
+# Assess test coverage
+go test -cover ./...
+
+# Check for static analysis issues "Does anything look suspicious?"
+go vet -v ./...
+
+# Are there bugs, bad practices or code smells?
+staticcheck ./...
